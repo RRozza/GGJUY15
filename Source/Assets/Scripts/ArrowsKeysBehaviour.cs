a6643509e0747fa43734a64263ca31cde39a7a38
Renzo <renzo.rozza@gmail.com>
2015-01-23 20:53:48 -0200

    ArrowsKeysBehaviour

---
+++ Source/Assets/Scripts/ArrowsKeysBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowsKeysBehaviour : MonoBehaviour {
+
+	public float speed = 1.5f;	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		}
+	}
+}

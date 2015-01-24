9d528c8c5eb2fa096c0c7230f396b6648305b85a
Plumouy <rodrigoarigon@gmail.com>
2015-01-24 18:43:02 -0300

    Parachute new movement

--- Source/Assets/Scripts/GameController.cs
+++ Source/Assets/Scripts/GameController.cs
@@ -1,48 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameController : MonoBehaviour {
-	public GameObject flyingObstacle;
-	public Vector3 spawnValues;
-	public int obstacleCount;
-	public float spawnWait;
-	public float startWait;
-	public float waveWait;
+public class GameController : MonoBehaviour
+{
+		public GameObject flyingObstacle;
+		public Vector3 spawnValues;
+		public int obstacleCount;
+		public float spawnWait;
+		public float startWait;
+		public float waveWait;
+		public int sec = 0;
+		public float timecount = 0;
+		public float starttime = 0;
+		public int parachutePopTime;
+		public int parachuteTrembleY;
+		public int parachuteAsensionSpeed;
+		private Vector3 parachuteStartPosition = new Vector3 (0, -6, 0);
 
-	public int sec=0;
-	public float timecount=0;
-	public float starttime=0;
-	public int parachutePopTime;
-
-	// Use this for initialization
-	void Start () {
-		starttime = Time.time;
-		StartCoroutine (SpawnWaves ());
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		timecount = Time.time - starttime;
-		sec = (int)(timecount % 60f);
-		if (sec == parachutePopTime) {
-			//parachute appears from down the screen
+		// Use this for initialization
+		void Start ()
+		{
+				starttime = Time.time;
+				StartCoroutine (SpawnWaves ());
+				GameObject parachute = Registry.Find ("Parachute");
+				//parachute.transform.position.x = 0;
+				//parachute.transform.position.y = 3;
+				parachute.transform.position = parachuteStartPosition;
+				Context.SharedInstance.trembleEnabled = false;
+			
 		}
-	}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				GameObject parachute = Registry.Find ("Parachute");
+				timecount = Time.time - starttime;
+				sec = (int)(timecount % 60f);
+				if (sec >= parachutePopTime) {
+						if (parachute.transform.position.y < parachuteTrembleY) {
+								parachute.transform.position += (Vector3.up * parachuteAsensionSpeed * Time.deltaTime);
+								//parachute appears from down the screen
+						} else if (parachute.transform.position.y >= parachuteTrembleY - 1) {
+								Context.SharedInstance.startParachuteTremble ();
+						}
+				}
+		}
 
-	IEnumerator SpawnWaves()
-	{
+		IEnumerator SpawnWaves ()
+		{
 
-		yield return new WaitForSeconds(startWait);
-		while (true) {
+				yield return new WaitForSeconds (startWait);
+				while (true) {
 						for (int i = 0; i < obstacleCount; i++) {
 								Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 								Quaternion spawnRotation = new Quaternion ();
 								Instantiate (flyingObstacle, spawnPosition, spawnRotation);
 								yield return new WaitForSeconds (spawnWait);
 						}
-			yield return new WaitForSeconds(waveWait);
+						yield return new WaitForSeconds (waveWait);
 				}
-	}
+		}
 	
 }
--- Source/Assets/Scripts/TremblingBehaviour.cs
+++ Source/Assets/Scripts/TremblingBehaviour.cs
@@ -1,29 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TremblingBehaviour : MonoBehaviour {
+public class TremblingBehaviour : MonoBehaviour
+{
 	
-	public float speed;
-	public int limit;
-	private int position = 0; 
-	private bool direction = true;
+		public float speed;
+		public int limit;
+		private int position = 0;
+		private bool direction = true;
+    
 	
-	// Update is called once per frame
-	void Update () {
-		if (position < (-1*limit)) {
-			direction = true;	
-		} 
-		if (position > limit) {
-			direction = false;
-		}
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Context.SharedInstance.trembleEnabled == true) {
+				
+						if (position < (-1 * limit)) {
+								direction = true;	
+						} 
+						if (position > limit) {
+								direction = false;
+						}
 		
-		if (direction) {
-			transform.position += (Vector3.left * speed * Time.deltaTime);
-			position++;
-		} else {
-			transform.position += (Vector3.right * speed * Time.deltaTime);
-			position--;
+						if (direction) {
+								transform.position += (Vector3.left * speed * Time.deltaTime);
+								position++;
+						} else {
+								transform.position += (Vector3.right * speed * Time.deltaTime);
+								position--;
+						}
+				}
 		}
-
-	}
 }

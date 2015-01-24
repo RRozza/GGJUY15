55f79f1a3111975a2f0128dc3293fd618327c6d5
Plumouy <rodrigoarigon@gmail.com>
2015-01-24 19:51:09 -0300

    Open Parachute Timer condition

--- Source/Assets/Scripts/ObstacleAsension.cs
+++ Source/Assets/Scripts/ObstacleAsension.cs
@@ -1,25 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ObstacleAsension : MonoBehaviour {
+public class ObstacleAsension : MonoBehaviour
+{
 
-	public float speed;
-	public int limit;
-	private int position = 0; 
-	private bool direction = true;
-	
-	public float xStart, yStart;
-	public float xMin, xMax, yMin, yMax;
-	public int screenTop;
+		public float speed;
+		public int limit;
+		private int position = 0;
+		private bool direction = true;
+		public float xStart, yStart;
+		public float xMin, xMax, yMin, yMax;
+		public int screenTop;
 
 	
-	// Use this for initialization
-	void Start () {
+		// Use this for initialization
+		void Start ()
+		{
 		
-	}
+		}
 	
-	// Update is called once per frame
-	void Update () {
-		transform.position += (Vector3.up * speed * Time.deltaTime);
-	}
+		// Update is called once per frame
+		void Update ()
+		{
+				if (this.transform.position.y <= screenTop + 3) {
+						transform.position += (Vector3.up * speed * Time.deltaTime);
+				}
+		}
 }

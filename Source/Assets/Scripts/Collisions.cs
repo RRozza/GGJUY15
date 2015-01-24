7d49e88468ff7e2f45826bb755c8faed47e3340a
Renzo <renzo.rozza@gmail.com>
2015-01-24 01:07:32 -0200

    Collisions

---
+++ Source/Assets/Scripts/Collisions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collisions : MonoBehaviour {
+
+	public int speed;
+	public int impulse;
+
+	void OnTriggerEnter2D (Collider2D collision){
+		if (collision.gameObject.name == "Player2") {
+			if (Input.GetKey (KeyCode.D)) {
+				GameObject.Find ("Player1").transform.position += (Vector3.right * speed * Time.deltaTime) * impulse;        
+			}
+
+			if (Input.GetKey (KeyCode.A)) {
+				GameObject.Find ("Player1").transform.position += (Vector3.left * speed * Time.deltaTime) * impulse;        
+			}
+		}
+	}
+}
---
+++ Source/Assets/Scripts/Collisions2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collisions2 : MonoBehaviour {
+
+	public int speed;
+	public int impulse;
+	
+	void OnTriggerEnter2D (Collider2D collision){
+		if (collision.gameObject.name == "Player1") {
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				GameObject.Find ("Player2").transform.position += (Vector3.right * speed * Time.deltaTime) * impulse;        
+			}
+			
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+				GameObject.Find ("Player2").transform.position += (Vector3.left * speed * Time.deltaTime) * impulse;        
+			}
+		}
+	}
+}

bc92b0c617f4b594e1b81d24e003c3b323ff23af
Renzo <renzo.rozza@gmail.com>
2015-01-24 05:55:31 -0200

    Super Merge

--- Source/Assets/Scripts/CollisionParachute.cs
+++ Source/Assets/Scripts/CollisionParachute.cs
@@ -6,7 +6,8 @@ public class CollisionParachute : MonoBehaviour
 	void OnTriggerEnter2D (Collider2D collision)
 	{
 		//Desactivacion al agarrar el parachute
-		if (collision.gameObject.CompareTag ("Player"))
+		if (collision.gameObject.CompareTag("Player")) {
 			gameObject.SetActive (false);	
+		}
 	}
 }
--- Source/Assets/Scripts/Collisions.cs
+++ Source/Assets/Scripts/Collisions.cs
@@ -32,26 +32,28 @@ public class Collisions : MonoBehaviour {
 	}
 	
 	void OnTriggerEnter2D (Collider2D collision){
-		Context.SharedInstance.collisioning = true;
-		bool p1AtTop = GameObject.Find("Player1").transform.position.y > GameObject.Find("Player2").transform.position.y;
-		bool p2AtTop = GameObject.Find("Player1").transform.position.y < GameObject.Find("Player2").transform.position.y;
-		
-		if (Input.GetKey(KeyCode.Keypad2) && p1AtTop) {
-			Context.SharedInstance.player2_mutex = true;
-		}
-		
-		if (Input.GetKey(KeyCode.H) && p2AtTop) {
-			Context.SharedInstance.player1_mutex = true;
-		}
+		if (collision.gameObject.CompareTag("Player")) {
+			Context.SharedInstance.collisioning = true;
+			bool p1AtTop = GameObject.Find ("Player1").transform.position.y > GameObject.Find ("Player2").transform.position.y;
+			bool p2AtTop = GameObject.Find ("Player1").transform.position.y < GameObject.Find ("Player2").transform.position.y;
 
-		int punch1 = Input.GetKey(KeyCode.Keypad1) ? 4 : 1;
-		int punch2 = Input.GetKey(KeyCode.G) ? 4 : 1;
-		if (GameObject.Find("Player1").transform.position.x > GameObject.Find("Player2").transform.position.x) {
-			GameObject.Find("Player1").transform.position += (Vector3.right * speed * Time.deltaTime) * impulse * punch2;
-			GameObject.Find("Player2").transform.position += (Vector3.left * speed * Time.deltaTime) * impulse * punch1;
-		} else {
-			GameObject.Find("Player1").transform.position += (Vector3.left * speed * Time.deltaTime) * impulse * punch2;
-			GameObject.Find("Player2").transform.position += (Vector3.right * speed * Time.deltaTime) * impulse * punch1;
+			if (Input.GetKey (KeyCode.Keypad2) && p1AtTop) {
+				Context.SharedInstance.player2_mutex = true;
+			}
+
+			if (Input.GetKey (KeyCode.H) && p2AtTop) {
+				Context.SharedInstance.player1_mutex = true;
+			}
+
+			int punch1 = Input.GetKey (KeyCode.Keypad1) ? 4 : 1;
+			int punch2 = Input.GetKey (KeyCode.G) ? 4 : 1;
+			if (GameObject.Find ("Player1").transform.position.x > GameObject.Find ("Player2").transform.position.x) {
+				GameObject.Find ("Player1").transform.position += (Vector3.right * speed * Time.deltaTime) * impulse * punch2;
+				GameObject.Find ("Player2").transform.position += (Vector3.left * speed * Time.deltaTime) * impulse * punch1;
+			} else {
+				GameObject.Find ("Player1").transform.position += (Vector3.left * speed * Time.deltaTime) * impulse * punch2;
+				GameObject.Find ("Player2").transform.position += (Vector3.right * speed * Time.deltaTime) * impulse * punch1;
+			}
 		}
 	}
 }

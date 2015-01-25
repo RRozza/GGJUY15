e7dc50b6d6081a37aa8bcdee37c7e0896987d941
Renzo <renzo.rozza@gmail.com>
2015-01-25 06:22:22 -0200

    GetParachute Animation Logic

--- Source/Assets/Scripts/CollisionParachute.cs
+++ Source/Assets/Scripts/CollisionParachute.cs
@@ -3,10 +3,20 @@ using System.Collections;
 
 public class CollisionParachute : MonoBehaviour {
 
+	private Animator anim;
+	private string animId;
+
+	void Start() {
+		anim = GetComponent<Animator> ();
+	}
+
 	void OnTriggerEnter2D (Collider2D collision) {
 		if (collision.gameObject.CompareTag("Player")) {
 			Context.SharedInstance.parachute_state = (collision.gameObject.name == "Player1") ? ParachuteState.P1 : ParachuteState.P2;
 			gameObject.SetActive(false);	
+
+			animId = (collision.gameObject.name == "Player1") ? "P1Parachute" : "P2Parachute";
+			anim.SetBool(animId, true);
 		}
 	}
 }

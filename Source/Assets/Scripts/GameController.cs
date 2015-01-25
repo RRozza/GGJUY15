de398ad2cd7d15c7dc9aa40cd240af8f5c055c37
tedybear26 <amy.chan.sucubo@gmail.com>
2015-01-25 04:01:03 -0200

    Super Merge

--- Source/Assets/Scripts/GameController.cs
+++ Source/Assets/Scripts/GameController.cs
@@ -2,106 +2,102 @@
 using System.Collections;
 using System.Collections.Generic;
 
-<<<<<<< .mine
 public class GameController : MonoBehaviour {
-=======
-public class GameController : MonoBehaviour
->>>>>>> .theirs
 
 	private List<GameObject> spawns = new List<GameObject> ();
 	private ArrayList  spawnsIds = new ArrayList();
+	private Vector3 parachuteStartPosition = new Vector3 (0, -6, 0);
+	private float starttime;
+	private float timecount;
 
+	public float parachutePopTime;
 	public Vector3 spawnValues;
 	public int obstacleCount;
 	public float spawnWait;
 	public float startWait;
 	public float spawnSpeed; 
-{
+	public int parachuteTrembleY;
+	public int parachuteAsensionSpeed;
+	public int parachuteOpenTime;
+	public int gameEndTime;
 
 
-		public int parachuteTrembleY;
-		public int parachuteAsensionSpeed;
-		public int parachuteOpenTime;
-		public int gameEndTime;
-		private Vector3 parachuteStartPosition = new Vector3 (0, -6, 0);
+	void Start() {
+		starttime = Time.time;
 
+		spawnsIds.Add ("Seat");
+		spawnsIds.Add ("Bag");
 
 		StartCoroutine (SpawnWaves ());
+
 		GameObject parachute = Registry.Find ("Parachute");
 		//parachute.transform.position.x = 0;
 		//parachute.transform.position.y = 3;
 		parachute.transform.position = parachuteStartPosition;
 		Context.SharedInstance.trembleEnabled = false;
 			
-		spawnsIds.Add ("Seat");
-		spawnsIds.Add ("Bag");
+
 	}
-	}
-		// Update is called once per frame
-		void Update ()
-		{		
-				foreach (GameObject spawn in spawns) {
-					spawn.transform.position += (Vector3.up * spawnSpeed * Time.deltaTime);	
 
-					if (spawn.transform.position.y > 7.0f) {
-						spawns.Remove(spawn);
-						//GameObject.Destroy(spawn);
-					}
-				}
+	// Update is called once per frame
+	void Update ()
+	{		
+		foreach (GameObject spawn in spawns) {
+			spawn.transform.position += (Vector3.up * spawnSpeed * Time.deltaTime);	
 
-				GameObject parachute = Registry.Find ("Parachute");
-				Players player = (gameObject.name == "Player1") ? Players.P1 : Players.P2;
-				ParachuteState playerGrabParachute = (player == Players.P1) ? ParachuteState.P1 : ParachuteState.P2;
-				timecount = Time.time - starttime;
-				Context.SharedInstance.updateTimer ((int)(timecount % 60f));
-				//Context.SharedInstance.timerSeconds = (int)(timecount % 60f);
-				if (Context.SharedInstance.timerSeconds >= parachutePopTime) {
-						if (parachute.transform.position.y < parachuteTrembleY) {
-								parachute.transform.position += (Vector3.up * parachuteAsensionSpeed * Time.deltaTime);
-								//parachute appears from down the screen
-						} else if (parachute.transform.position.y >= parachuteTrembleY - 1) {
-								Context.SharedInstance.startParachuteTremble ();
-						}
-				}
-				if (Context.SharedInstance.timerSeconds >= parachuteOpenTime) {
-						Context.SharedInstance.enableParachuteOpening ();
-						if (Context.SharedInstance.isKeyPress (player, Keys.PUNCH) && (Context.SharedInstance.parachute_state == playerGrabParachute)) {
-								if (Context.SharedInstance.parachuteIsOpened == false) {				
-										OpenParachute ();
-										EndGame (player.ToString ());
-								}
-						}
-				}
-				if (Context.SharedInstance.timerSeconds == this.gameEndTime && Context.SharedInstance.gameWinner != "") {
-						//GAME END LOGIC
-						this.EndGame ("none");
-						//CALCULATE ROUND WINNER
-						//UPDATE SCORES
-						//SHOW ENDING
-						//START NEXT GAME
-
-				}
+			if (spawn.transform.position.y > 7.0f) {
+				spawns.Remove(spawn);
+				//GameObject.Destroy(spawn);
+			}
 		}
 
-
-		
-		
-
-
-
-		void EndGame (string winner)
-		{
-				if (winner == "none") {
-						//both lose
-						Debug.Log ("BOTH PLAYERS LOSE");
-				} else {
-						Context.SharedInstance.winGame (winner);
+		GameObject parachute = Registry.Find ("Parachute");
+		Players player = (gameObject.name == "Player1") ? Players.P1 : Players.P2;
+		ParachuteState playerGrabParachute = (player == Players.P1) ? ParachuteState.P1 : ParachuteState.P2;
+		timecount = Time.time - starttime;
+		Context.SharedInstance.updateTimer ((int)(timecount % 60f));
+		//Context.SharedInstance.timerSeconds = (int)(timecount % 60f);
+		if (Context.SharedInstance.timerSeconds >= parachutePopTime) {
+			if (parachute.transform.position.y < parachuteTrembleY) {
+				parachute.transform.position += (Vector3.up * parachuteAsensionSpeed * Time.deltaTime);
+				//parachute appears from down the screen
+			} else if (parachute.transform.position.y >= parachuteTrembleY - 1) {
+				Context.SharedInstance.startParachuteTremble ();
+			}
+		}
+		if (Context.SharedInstance.timerSeconds >= parachuteOpenTime) {
+			Context.SharedInstance.enableParachuteOpening ();
+			if (Context.SharedInstance.isKeyPress (player, Keys.PUNCH) && (Context.SharedInstance.parachute_state == playerGrabParachute)) {
+				if (Context.SharedInstance.parachuteIsOpened == false) {				
+					OpenParachute ();
+					EndGame (player.ToString ());
 				}
+			}
+		}
+		if (Context.SharedInstance.timerSeconds == this.gameEndTime && Context.SharedInstance.gameWinner != "") {
+			//GAME END LOGIC
+			this.EndGame ("none");
+			//CALCULATE ROUND WINNER
+			//UPDATE SCORES
+			//SHOW ENDING
+			//START NEXT GAME
+			
+		}
+	}
 
-				//if with parachute and parachute is opened -->one winner
-				
-				//if with parachute but not opened --> both lose
-				//if no one has parachute --> both lose
+	void EndGame (string winner)
+	{
+		if (winner == "none") {
+				//both lose
+				Debug.Log ("BOTH PLAYERS LOSE");
+		} else {
+				Context.SharedInstance.winGame (winner);
+		}
+
+		//if with parachute and parachute is opened -->one winner
+		
+		//if with parachute but not opened --> both lose
+		//if no one has parachute --> both lose
 	}
 
 	IEnumerator SpawnWaves()
@@ -118,17 +114,18 @@ public class GameController : MonoBehaviour
 
 			yield return new WaitForSeconds(spawnWait);
 		}
+	}
 
-		void OpenParachute ()
-		{
-				Context.SharedInstance.parachuteIsOpened = true;
-				this.DisablePlayerControls ();
-		}
+	void OpenParachute ()
+	{
+		Context.SharedInstance.parachuteIsOpened = true;
+		this.DisablePlayerControls ();
+	}
 
-		void DisablePlayerControls ()
-		{
+	void DisablePlayerControls ()
+	{
 
 
-		}
+	}
 	
 }
